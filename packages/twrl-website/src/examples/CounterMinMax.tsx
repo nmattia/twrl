4539b4c6e058ef44681f9162f84b81595349af58
Nicolas Mattia <nicolas@nmattia.com>
2025-06-10 22:58:23 +0200

    Add Trigger.set

--- packages/twrl-website/src/examples/CounterMinMax.tsx
+++ packages/twrl-website/src/examples/CounterMinMax.tsx
@@ -1,16 +1,23 @@
 import { Trigger } from "twrl";
 
+// Constants
+const MIN = 1;
+const MAX = 5;
+const START = MIN;
+
+// Helpers
+const clamp = (x: number) => Math.min(Math.max(x, MIN), MAX);
+const inc = (x: number) => x + 1;
+const dec = (x: number) => x - 1;
+
+// Component
+// prettier-ignore
 export const CounterMinMax = () => {
-  const plus = new Trigger();
-  const minus = new Trigger();
-  const plusf = plus.map((_) => (x: number) => x + 1);
-  const minusf = minus.map((_) => (x: number) => x - 1);
-  const MAX = 5,
-    MIN = 1;
-  const clamp = (x: number) => Math.min(Math.max(x, MIN), MAX);
+  const plus = new Trigger(), plusf = plus.set(inc);
+  const minus = new Trigger(), minusf = minus.set(dec);
   const value = Trigger.any(plusf, minusf).track(
     (acc, f) => clamp(f(acc)),
-    MIN,
+    START,
   );
   return (
     <div>
